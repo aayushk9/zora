ff8c53c6095af2944e1d9e1c7650decd9effcbf2
Aayush <ayushkokate1@gmail.com>
2025-11-08 21:26:27 +0530

    refactor: restructure app with path/route and pages

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,16 +5,15 @@ import Agents from './pages/Agents';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
-         <Routes>
-           <Route path='/' element={Zora} />
-           <Route path="/query" element={Query} />
-           <Route path='/query/:id' element={Query} />
-           <Route path="/agents" element={Agents}/>
-         </Routes>
+        <Routes>
+          <Route path='/' element={Zora} />
+          <Route path="/query" element={Query} />
+          <Route path='/query/:id' element={Query} />
+          <Route path="/agents" element={Agents} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
