791c40c292b882504502d4f7415ffb10c3af878a
Aayush <ayushkokate1@gmail.com>
2025-11-10 21:20:03 +0530

    midway

--- apps/web/src/pages/Zora.tsx
+++ apps/web/src/pages/Zora.tsx
@@ -1,8 +1,54 @@
-export default function Zora () {
-    
+import React from "react";
+import { Navbar } from "../components/Navbar/Navbar.tsx"
+
+export default function Zora() {
+
     return (
-        <div>
-         {/* import components here*/}
-        </div>
+        <React.Fragment>
+            <Navbar />
+            <div style={{
+                border: "1px solid rgba(255, 255, 255, 0.2)",
+                borderRadius: "8px",
+                height: "94vh",
+                width: "99vw",
+                boxSizing: "border-box",
+                overflow: "hidden",
+                overflowY: "auto",
+                marginTop: "4px"
+            }}>
+
+                <div
+                    style={{
+                        height: "100%",
+                        width: "100%",
+                        overflowY: "auto",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "flex-start",
+                        paddingTop: "20px",
+                    }}
+                >
+
+                    <span style={{
+                        border: "1px solid rgba(255, 255, 255, 0.6)",
+                        borderRadius: "20px",
+                        color: "white",
+                        fontSize: "14px",
+                        letterSpacing: "1px",
+                        padding: "8px 16px",
+                        textTransform: "uppercase",
+                        
+                        backdropFilter: "blur(6px)",
+                    }}>
+                        currenlty supporting polymarket
+                    </span>
+
+                    <h1>nmnmnmnmmnmdme</h1>
+                </div>
+
+                {/* import input box and events here */}
+            </div>
+        </React.Fragment>
     )
 }
