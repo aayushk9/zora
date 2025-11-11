f035d8830b1de9b4aca3a95991d12d1c8a24dbea
Aayush <ayushkokate1@gmail.com>
2025-11-11 21:26:12 +0530

    feat(ui): replace input with textarea and some style updates

--- apps/web/src/components/InputBox/InputBox.tsx
+++ apps/web/src/components/InputBox/InputBox.tsx
@@ -1,14 +1,27 @@
 import React from "react"
 import styles from './InputBox.module.css'
+import { useState } from "react"
+import sendIcon from '../../assets/send.png'
 
-export function InputBox () {
+export function InputBox() {
+
+    const [text, setText] = useState("")
+
     return (
         <React.Fragment>
-          
-           <div className={styles.container}>
-             <textarea className={styles.input} placeholder="Describe your trading strategy idea"/>
-           </div>
-          
+            <div className={styles.container}>
+                <div>
+                   <textarea 
+                   value={text}
+                   onChange={(e) => setText(e.target.value)}
+                   className={styles.input} 
+                   placeholder="Describe your trading strategy idea"
+                   />
+                  <button className={styles.sendBtn}>
+        <img src={sendIcon} alt="send" className={styles.sendIcon} />
+      </button>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
