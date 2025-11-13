26b990b6052240fce3346145e690afb28bcdeda3
Aayush <ayushkokate1@gmail.com>
2025-11-13 21:16:13 +0530

    refactor: setup initial layout of the page

--- apps/web/src/components/GeneralPlan/GeneralPlan.tsx
+++ apps/web/src/components/GeneralPlan/GeneralPlan.tsx
@@ -1,9 +1,19 @@
 import React from "react"
+import styles from './GeneralPlan.module.css'
 
 export function GeneralPlan () {
     return (
         <React.Fragment>
+           <div className={styles.parentContainer}>
+              <div className={styles.chatbox}>
+                <div className={styles.query}>
 
+                </div>
+                <div className={styles.execution}>
+
+                </div>
+              </div>
+           </div>
         </React.Fragment>
     )
 }
