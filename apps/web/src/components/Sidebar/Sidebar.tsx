2eb4eb89620ae92eb92f052f8e92c60e8550ce10
Aayush <ayushkokate1@gmail.com>
2025-11-13 21:15:42 +0530

    refactor: setup initial layout of tha page

--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
 import React from "react"
+import styles from "./Sidebar.module.css"
 
 export function Sidebar () {
     return (
         <React.Fragment>
-
+           <div className={styles.parentContainer}>
+             
+           </div>
         </React.Fragment>
     )
 }
