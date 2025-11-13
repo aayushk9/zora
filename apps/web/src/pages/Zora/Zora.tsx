58b0e8d0ba4c4e516c3ea4475d0f9c0a30df725d
Aayush <ayushkokate1@gmail.com>
2025-11-14 05:29:48 +0530

    style: resize font and manage color combinations

--- apps/web/src/pages/Zora/Zora.tsx
+++ apps/web/src/pages/Zora/Zora.tsx
@@ -3,7 +3,6 @@ import { Navbar } from "../../components/Navbar/Navbar.tsx"
 import  { InputBox } from '../../components/InputBox/InputBox.tsx'
 import { Events } from "../../components/Events/Events.tsx"
 import styles from './Zora.module.css'
-import { Beta } from "../../components/Beta/Beta.tsx";
 
 export default function Zora() {
 
@@ -16,15 +15,18 @@ export default function Zora() {
                         supporting jupiter prediction market
                     </span>
                     <h1 className={styles.mainHeader}>
-                        Ask Zora to Trade <span>Prediction Market</span>
+                        Ask Zora to {""}
+                        <span className={styles.trade}>
+                            Trade
+                        </span> {""}
+                        Prediction Markets
                     </h1>
                     <span className={styles.foot}>
                         Build prediction market strategies in minutes with a single prompt
-                    </span>   
+                    </span>
                 </div>
-            
-                  <InputBox/>
-                  <Events/>
+                <InputBox/>
+                <Events/>
             </div>
         </React.Fragment>
     )
