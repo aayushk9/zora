ba7546cbaa928b63c747c7251d4967fe7b9475a8
Aayush <ayushkokate1@gmail.com>
2025-11-13 21:14:57 +0530

    start query page development

--- apps/web/src/pages/Query.tsx
+++ apps/web/src/pages/Query.tsx
@@ -1,8 +1,15 @@
+import React from "react";
+import { Navbar } from "../components/Navbar/Navbar";
+import { GeneralPlan } from "../components/GeneralPlan/GeneralPlan";
+import { Sidebar } from "../components/Sidebar/Sidebar";
+
 export default function Query () {
     
     return (
-        <div>
-         {/* import components here*/}
-        </div>
+       <React.Fragment>
+           <Navbar/>
+           <GeneralPlan/>
+           <Sidebar/>
+       </React.Fragment>
     )
 }
