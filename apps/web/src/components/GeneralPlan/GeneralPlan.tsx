ff981e2c515bdd3378e5d3d722936987defa9369
Aayush <ayushkokate1@gmail.com>
2025-11-14 15:18:18 +0530

    style: some minor ui changes

--- apps/web/src/components/GeneralPlan/GeneralPlan.tsx
+++ apps/web/src/components/GeneralPlan/GeneralPlan.tsx
@@ -10,17 +10,19 @@ export function GeneralPlan () {
               <div id="sidebar">
                <Sidebar/>
               </div>
-
               <div id="queryExecutionBox" className={styles.queryExecutionBox}>
                    <div id="query" className={styles.query}>
                         <h1>Query</h1>
+                        <div className={styles.queryBorder}></div>
                         <div className={styles.inputBox}>
                           <InputBox/>
                         </div>
                    </div>
-
+                 
+                 <div className={styles.executionBorder}></div>
                    <div id="execution" className={styles.execution}>
                       <h1>Execution</h1>
+                      
                    </div>
               </div>
            </div>
