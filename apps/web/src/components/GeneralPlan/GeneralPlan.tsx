53849be1c4162da41c076b77d060ac030046515d
Aayush <ayushkokate1@gmail.com>
2025-11-14 14:52:53 +0530

    feat(ui): style and structure updates

--- apps/web/src/components/GeneralPlan/GeneralPlan.tsx
+++ apps/web/src/components/GeneralPlan/GeneralPlan.tsx
@@ -1,20 +1,27 @@
 import React from "react"
 import styles from './GeneralPlan.module.css'
 import { InputBox } from "../InputBox/InputBox"
+import { Sidebar } from "../Sidebar/Sidebar"
 
 export function GeneralPlan () {
     return (
         <React.Fragment>
            <div className={styles.parentContainer}>
-              <div className={styles.chatbox}>
-                <div className={styles.query}>
-                  <div className={styles.inputBox}>
-                    <InputBox/>
-                  </div>
-                </div>
-                <div className={styles.execution}>
+              <div id="sidebar">
+               <Sidebar/>
+              </div>
 
-                </div>
+              <div id="queryExecutionBox" className={styles.queryExecutionBox}>
+                   <div id="query" className={styles.query}>
+                        <h1>Query</h1>
+                        <div className={styles.inputBox}>
+                          <InputBox/>
+                        </div>
+                   </div>
+
+                   <div id="execution" className={styles.execution}>
+                      <h1>Execution</h1>
+                   </div>
               </div>
            </div>
         </React.Fragment>
