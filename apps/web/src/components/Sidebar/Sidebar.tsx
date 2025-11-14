c513cfaae67891ec4045bc34a58049f5bd565a97
Aayush <ayushkokate1@gmail.com>
2025-11-14 14:52:16 +0530

    align and style sidebar

--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,13 @@ export function Sidebar () {
     return (
         <React.Fragment>
            <div className={styles.parentContainer}>
-             
+             <div className={styles.newChat}>
+               <button className={styles.button}>Chat</button>
+               <button className={styles.button}>Agents</button>
+             </div>
+             <div className={styles.chatHistory}>
+                <span className={styles.history}>History</span>
+             </div>
            </div>
         </React.Fragment>
     )
