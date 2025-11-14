982cb8ab1a63cc2194ec31cc3d5ba9dd2737b98d
Aayush <ayushkokate1@gmail.com>
2025-11-14 18:25:34 +0530

    feat(ui): structure the sidebar and its colors

--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,15 @@
 import React from "react"
 import styles from "./Sidebar.module.css"
+import { Link } from "react-router-dom"
 
 export function Sidebar () {
     return (
         <React.Fragment>
            <div className={styles.parentContainer}>
-             <div className={styles.newChat}>
-               <button className={styles.button}>Chat</button>
-               <button className={styles.button}>Agents</button>
-             </div>
-             <div className={styles.chatHistory}>
-                <span className={styles.history}>History</span>
+             <div className={styles.sidebar}>
+               <Link to="/query" className={styles.chat}>Chat</Link>
+               <Link to="/agents" className={styles.agents}>Agents</Link>
+               <Link to="/query" className={styles.history}>History</Link> {/* for history there will be dynamic route such as /query/chatid */}
              </div>
            </div>
         </React.Fragment>
