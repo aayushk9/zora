f9bb7fa0158a712fb1a6bf6891238784ba760849
Aayush <ayushkokate1@gmail.com>
2025-11-14 21:28:00 +0530

    feat(page): structure and design the query page

--- apps/web/src/components/GeneralPlan/GeneralPlan.tsx
+++ apps/web/src/components/GeneralPlan/GeneralPlan.tsx
@@ -4,37 +4,38 @@ import { InputBox } from "../InputBox/InputBox"
 import { Sidebar } from "../Sidebar/Sidebar"
 import DottedBackground from "../DottedBackground/DottedBackground"
 
-export function GeneralPlan () {
-    return (
-        <React.Fragment>
-           <div className={styles.parentContainer}>
-              <div id="sidebar">
-               <Sidebar/>
-              </div>
-              <div id="queryExecutionBox" className={styles.queryExecutionBox}>
-                   <div id="query" className={styles.queryBox}>
-                     <br/>
-                        <span className={styles.queryHeader}>Query</span>
-                        <br/>
-                        <br/>
-                        <div className={styles.queryBorder}></div>
-                        <div className={styles.inputBox}>
-                          <InputBox noOuterBorder/>
-                        </div>
-                   </div>
-                 
-                   <div id="execution" className={styles.executionBox}>
-                     <br/>
-                      <span className={styles.executionHeader}>Execution</span>
-                      <br/>
-                      <br/>
-                       <div className={styles.executionBorder}></div>
-                       <div className={styles.executionBody}>
-                       <DottedBackground/>
-                       </div>
-                   </div>
-              </div>
-           </div>
-        </React.Fragment>
-    )
+export function GeneralPlan() {
+   return (
+      <React.Fragment>
+         <div className={styles.parentContainer}>
+            <div id="sidebar">
+               <Sidebar />
+            </div>
+            <div id="queryExecutionBox" className={styles.queryExecutionBox}>
+               
+               <div id="query" className={styles.queryBox}>
+                  <br />
+                  <span className={styles.queryHeader}>Query</span>
+                  <br />
+                  <br />
+                  <div className={styles.queryBorder}></div>
+                  <div className={styles.inputBox}>
+                     <InputBox noOuterBorder noSuggestedPrompts/>
+                  </div>
+               </div>
+
+               <div id="execution" className={styles.executionBox}>
+                  <br />
+                  <span className={styles.executionHeader}>Execution</span>
+                  <br />
+                  <br />
+                  <div className={styles.executionBorder}></div>
+                  <div className={styles.executionBody}>
+                     <DottedBackground />
+                  </div>
+               </div>
+            </div>
+         </div>
+      </React.Fragment>
+   )
 }
