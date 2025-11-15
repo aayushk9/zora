6921af97905146cd0f221f69b677c8f662e589be
Aayush <ayushkokate1@gmail.com>
2025-11-15 06:34:23 +0530

    style: space navs between and minor changes

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
 import React from 'react'
 
@@ -5,10 +6,13 @@ export function Navbar() {
 
     return (
         <React.Fragment>
-          <div className={styles.parentContainer}>
-            <span className={styles.zora}>Zora</span>
-            <button className={styles.signin}>Sign in</button>
-          </div>
+          <nav className={styles.navbar}>
+            <div> <Link className={styles.zora} to="/"> Zora </Link></div>
+            <div className={styles.navRight}>
+              <Link className={styles.agents} to="/agents">Tools</Link>
+              <Link className={styles.signin} to="/">Sign in</Link>
+            </div>
+          </nav>
         </React.Fragment>
     )
 }
