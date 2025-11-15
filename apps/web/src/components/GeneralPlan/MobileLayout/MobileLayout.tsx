9496842b3f227604450938df39c6d294c09a89b8
Aayush <ayushkokate1@gmail.com>
2025-11-15 18:44:50 +0530

    style: create mobile specific page for query pagr

--- apps/web/src/components/GeneralPlan/MobileLayout/MobileLayout.tsx
+++ apps/web/src/components/GeneralPlan/MobileLayout/MobileLayout.tsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './MobileLayout.module.css'
+import { InputBox } from "../../InputBox/InputBox";
 
 export function MobileLayout () {
 
+    const [query, setQuery] = useState(true);
+    const [execution, setExecution] = useState(false);
+
+    const handleQuery = () => {
+        setQuery(true);
+        setExecution(false);
+    }
+    
+    const handleExecution = () => {
+        setQuery(false);
+        setExecution(true);
+    }
+
     return (
         <React.Fragment>
-            <h1>Mobile Page</h1>
+            <div className={styles.parentContainer}>
+              <div className={styles.toggle}>
+                 <button
+                   onClick={handleQuery}
+                  className={styles.query}>
+                    Query
+                  </button>
+                 <button 
+                  onClick={handleExecution}
+                  className={styles.execution}>
+                    Execution
+                 </button>
+               </div>
+
+              <div className={styles.queryEexecutionPanel}>
+
+                {query && (
+                  <div className={styles.queryPanel}>
+
+                    <div className={styles.inputBox}><InputBox noSuggestedPrompts/></div>
+                  </div>
+                )}
+
+                {execution && (
+                    <div>
+                        <h1 style={{color: "white"}}>fkenfekfnekf</h1>
+                    </div>
+                )}
+              </div>
+            </div>
         </React.Fragment>
     )
 }
