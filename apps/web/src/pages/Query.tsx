fc22ffa5722e715edafd61afef8515276c77d87f
Aayush <ayushkokate1@gmail.com>
2025-11-15 18:45:51 +0530

    include conditional logic for screens

--- apps/web/src/pages/Query.tsx
+++ apps/web/src/pages/Query.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
 import { GeneralPlan } from "../components/GeneralPlan/GeneralPlan";
+import { useIsMobile } from "../components/useIsMobile/useIsMobile";
 
-export default function Query () {
-    
+export default function Query() {
+    const isMobile = useIsMobile(768);
     return (
-       <React.Fragment>
-           <Navbar/>
-           <GeneralPlan/>
-       </React.Fragment>
+        <React.Fragment>
+            {isMobile ? (
+                <GeneralPlan />
+            ) : (
+                <>
+                    <Navbar />
+                    <GeneralPlan />
+                </>
+            )}
+
+        </React.Fragment>
     )
 }
