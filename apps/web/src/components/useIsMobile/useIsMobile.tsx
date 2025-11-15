93813390c52caaec5709addd5e580391ca9bad54
Aayush <ayushkokate1@gmail.com>
2025-11-15 13:33:47 +0530

    refactor: add mobile reponsive logic

---
+++ apps/web/src/components/useIsMobile/useIsMobile.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export function useIsMobile (breakpoint: number = 768) {
+
+    const [isMobile, setIsMobile] = useState<boolean>(
+        window.innerWidth < 768
+    )
+
+    useEffect( () => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < breakpoint)
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+
+    }, [breakpoint])
+
+    return isMobile;
+   
+}
