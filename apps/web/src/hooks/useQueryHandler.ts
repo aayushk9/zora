48d7cc89913796420c1ea87d574f7e685827a480
Aayush <ayushkokate1@gmail.com>
2025-11-18 18:25:17 +0530

    replace multiple state with state[] for messages pair

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -1,16 +1,42 @@
 import { useState } from "react";
 
+interface Message {
+    role: "user" | "agent",
+    content: string
+}
+
 export function useQueryHandler () {
-    const [queries, setQueries] = useState<string[]>([]);
-    const [responses, setResponses] = useState<string[]>([]);
+    const [messages, setMessages] = useState<Message[]>([]);
+    const handleUserQuery = async(input: string) => {
+       
+        const updatedMessage: Message[] = [
+            ...messages, 
+            {
+                role: "user",
+                content: input
+            }
+        ]
 
+        setMessages(updatedMessage)
 
-    const handleUserQuery = (input: string) => {
-        setQueries(prev => [...prev, input]);
+        const res = await fetch(`http://localhost:3000/chat`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedMessage)
+        })
 
-        // bunch of api request back and fourth
+        const data = await res.json();
+
+        setMessages(prev => [
+            ...prev, 
+            {
+                role: 'agent',
+                content: data.reply
+            }
+        ])
         
-        setResponses(prev => [...prev])
     }
-    return {queries, responses, handleUserQuery, setQueries}
+    return {handleUserQuery, messages, setMessages}
 }
