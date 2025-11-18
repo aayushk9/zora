e401291fd73cd150c303f9d8982fa1fc55e90b01
Aayush <ayushkokate1@gmail.com>
2025-11-18 10:04:53 +0530

    query works now

--- apps/web/src/components/GeneralPlan/DesktopLayout/DesktopLayout.tsx
+++ apps/web/src/components/GeneralPlan/DesktopLayout/DesktopLayout.tsx
@@ -1,49 +1,62 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from './DesktopLayout.module.css'
 import { Sidebar } from "../../Sidebar/Sidebar";
 import { InputBox } from "../../InputBox/InputBox";
 import DottedBackground from "../../DottedBackground/DottedBackground";
+import { useLocation } from "react-router-dom";
 
-export function DesktopLayout () {
-      
+
+export function DesktopLayout() {
+
+   const location = useLocation();
+   const params = new URLSearchParams(location.search);
+   const incomingText = params.get("text");
    const [query, setQuery] = useState("")
    const [response, setResponse] = useState("");
 
-   const handleUserQuery = (input: string) => {
-   // stored user input in query state
-    setQuery(input);
+   useEffect(() => {
+      if (incomingText) {
+         setQuery(incomingText)
+      }
+   }, [incomingText])
 
-    // now as we have user requested input in our local query state variable we can send a POST
-    // request to our backend in form of body: query and exprect response from server and update
-    // in our reposne as setResponse(backend from backend)
-    setResponse("hadr coded resposne from developer")
+   const handleUserQuery = (input: string) => {
+      // stored user input in query state
+      setQuery(input);
+
+      // now as we have user requested input in our local query state variable we can send a POST
+      // request to our backend in form of body: query and exprect response from server and update
+      // in our reposne as setResponse(backend from backend)
+      setResponse("hadr coded resposne from developer")
    }
 
 
-    return (
-        <React.Fragment>
-            <div className={styles.parentContainer}>
+   return (
+      <React.Fragment>
+         <div className={styles.parentContainer}>
             <div id="sidebar">
                <Sidebar />
             </div>
             <div id="queryExecutionBox" className={styles.queryExecutionBox}>
-               
+
                <div id="query" className={styles.queryBox}>
                   <br />
                   <span className={styles.queryHeader}>Query</span>
                   <br />
                   <br />
                   <div className={styles.queryBorder}></div>
-                   <div className={styles.userInterface}>
-                      <div>
-                         {query}
-                      </div>
-                      <div>
-                       {response}
-                      </div>
+                  <div className={styles.userInterface}>
+                     <div>
+                        <p style={{
+                           color: "white"
+                        }}>{query}</p>
+                     </div>
+                     <div>
+                        {response}
+                     </div>
                   </div>
                   <div className={styles.inputBox}>
-                     <InputBox noOuterBorder noSuggestedPrompts onSend={handleUserQuery}/>
+                     <InputBox noOuterBorder noSuggestedPrompts onSend={handleUserQuery} />
                   </div>
                </div>
 
@@ -59,6 +72,6 @@ export function DesktopLayout () {
                </div>
             </div>
          </div>
-        </React.Fragment>
-    )
+      </React.Fragment>
+   )
 }
