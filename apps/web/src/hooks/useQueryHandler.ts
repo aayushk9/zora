675b34e1679bbe0a244915553c05daa5d4ebddd4
Aayush <ayushkokate1@gmail.com>
2025-11-18 23:43:53 +0530

    some modifications

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -1,42 +1,47 @@
 import { useState } from "react";
 
 interface Message {
-    role: "user" | "agent",
-    content: string
+    role: "user" | 'agent',
+    content: string 
 }
 
-export function useQueryHandler () {
+export function useQueryHandler() {
+    const [input, setInput] = useState("");
     const [messages, setMessages] = useState<Message[]>([]);
-    const handleUserQuery = async(input: string) => {
-       
-        const updatedMessage: Message[] = [
-            ...messages, 
-            {
-                role: "user",
-                content: input
-            }
-        ]
 
-        setMessages(updatedMessage)
 
-        const res = await fetch(`http://localhost:3000/chat`,{
+    const handleUserQuery = async () => {
+
+        if (!input.trim()) return;
+
+        const userMessage: Message = { role: "user", content: input }
+
+        setMessages((prev) => [...prev, userMessage]);
+        setInput("")
+
+        const res = await fetch(`http://localhost:3000/chat`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(updatedMessage)
+            body: JSON.stringify(messages)
         })
 
         const data = await res.json();
 
-        setMessages(prev => [
-            ...prev, 
-            {
-                role: 'agent',
-                content: data.reply
-            }
-        ])
-        
+        const agentMessage: Message = { role: 'agent', content: "hard coded for now instead of server message"}
+
+        setMessages(prev => [...prev, agentMessage])
+
+        //const data = await res.json();
+
     }
-    return {handleUserQuery, messages, setMessages}
+
+     return {
+    input,
+    setInput,
+    messages,
+    setMessages,
+    handleUserQuery,
+  };
 }
