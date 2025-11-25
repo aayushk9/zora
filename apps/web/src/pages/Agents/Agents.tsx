e7902256ad1e76cae60dcd339a5991288306d679
Aayush <ayushkokate1@gmail.com>
2025-11-25 21:56:18 +0530

    feat(web): replace agent file with folder for parent border

---
+++ apps/web/src/pages/Agents/Agents.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../../components/Navbar/Navbar";
+import { AgentNetwork } from "../../components/AgentNetwork/AgentNetwork";
+import styles from './Agents.module.css'
+
+export default function Agents () {
+  
+    return (
+          <div className={styles.parent}>
+             <Navbar/>
+             <AgentNetwork/>
+          </div>
+    )
+}
