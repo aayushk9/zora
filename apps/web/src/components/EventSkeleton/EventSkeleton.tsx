48abbab5f162935b77e9eff8c97d58e2a45dbb9d
Aayush <ayushkokate1@gmail.com>
2025-12-29 21:35:52 +0530

    feat: add empty event body when events are not fetched from api

---
+++ apps/web/src/components/EventSkeleton/EventSkeleton.tsx
@@ -0,0 +1,18 @@
+import styles from "./EventSkeleton.module.css";
+
+export function EventSkeleton() {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <div className={styles.image} />
+        <div className={styles.title} />
+      </div>
+
+      <div className={styles.lines}>
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+}
