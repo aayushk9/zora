9106d30847147440d19b3f887f3d97316fd6dc00
Aayush <ayushkokate1@gmail.com>
2025-12-30 20:21:09 +0530

    feat: complete query interface

--- apps/web/src/components/GeneralPlan/DesktopLayout/DesktopLayout.tsx
+++ apps/web/src/components/GeneralPlan/DesktopLayout/DesktopLayout.tsx
@@ -43,6 +43,29 @@ export function DesktopLayout() {
                         {messages.map((message, index) => (
                            <p key={index} className={`${message.role == "user" ? styles.userQuery : styles.agentResponse}`}>
                             {message.content}
+                            {/* currently if we are selecting event and navigating to query page the selected event is attached to 
+                                query, but it does not look good with i/p box, instead we can attach selected event below the first 
+                                user query
+
+                                // how do we achive it?
+                                1. we will be using useSelectedEvent store here
+                                2. as we redirect from / to /query, we will be removing selected event from inputbox
+                                3. add selected event to first message by the user
+
+                                loader?
+                                currenlty we are hard coding message on behalf of agent, but when the server will be activated the repsone time
+                                will be increased, therefore loader is needed
+
+                                how do we add a loader?
+                                1. add a loader state
+                                2. give a default value of false
+                                3. when the user sends query change state value to true
+                                4. when loader -> show loader else -> response
+
+                                5. if message.role == "user" && message.content == input -> setLoader(true)
+                                6. as loader is true display loader 
+                                7. when agent response is ready set loader to false and show agent response
+                            */}
                           </p>
                         ))}
                         <div ref={messagesEndRef} />
