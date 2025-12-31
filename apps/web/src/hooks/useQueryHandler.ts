b1494a4446e36641cade8393be1545f382105308
Aayush <ayushkokate1@gmail.com>
2025-12-31 19:42:33 +0530

    add console messages for debugging

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -22,17 +22,15 @@ export function useQueryHandler() {
     }, [incomingText])
 
     const handleUserQuery = async (input: string) => {
-
         if (!input.trim()) return;
-
         const userMessage: Message = { role: "user", content: input }
-
         let updatedMessages: Message[] = [];
+        
         setMessages(prev => {
             updatedMessages = [...prev, userMessage];
             return updatedMessages;
         });
-
+        console.log(updatedMessages)
 
         try {
             const res = await fetch(`http://localhost:3000/chat`, {
@@ -42,17 +40,13 @@ export function useQueryHandler() {
                 },
                 body: JSON.stringify([updatedMessages])
             })
-            // after server is ready do =>
-            //const data = await res.json();
-            // store data in agent message as
-            // data.reply => agentMessage = {role: "agent", content: data.reply}
          console.log(res)
         } catch (err) {
             console.log(err)
         }
 
         // hard coding agent response as of now until server is up
-        const agentMessage: Message = { role: 'agent', content: "hard coded for now instead of server message"}
+        const agentMessage: Message = { role: 'agent', content: "hard coded for now instead of server respose"}
         setMessages(prev => [...prev, agentMessage])
     }
 
