5471d3dac157c6dd96f68bbde4e6ccddb47b3b1a
Aayush <ayushkokate1@gmail.com>
2026-01-04 18:25:25 +0530

    import event,generate prompts modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { GeneratePromptsModule } from './generate-prompts/generate-prompts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, GeneratePromptsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
