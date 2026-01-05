3c528e599febbbc9f58b3fe7a0a81deaa14afc43
Aayush <ayushkokate1@gmail.com>
2026-01-05 21:31:34 +0530

    feat(api): set boilerplate code code calling openai

--- apps/api/src/chat/chat.controller.ts
+++ apps/api/src/chat/chat.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post, Body } from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { SelectedEventsDto } from 'src/generate-prompts/dto/selected-events.dto';
+import { userRequest } from './dto/userRequest.dto';
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+
+    constructor(private readonly chatService: ChatService) {}
+
+    @Post()
+    sendResponse(@Body('userQuery, selectedEvents') userQuery: userRequest[], selectedEvents: SelectedEventsDto[]){
+       this.chatService.fetchResponse(userQuery, selectedEvents)
+    }
+}
--- apps/api/src/chat/chat.service.ts
+++ apps/api/src/chat/chat.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { userRequest } from './dto/userRequest.dto';
+import { SelectedEventsDto } from 'src/generate-prompts/dto/selected-events.dto';
+import { throwError } from 'rxjs';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+    constructor(private openai: OpenAI) {
+      this.openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+      })
+    }
+    
+    async fetchResponse(userQuery: userRequest[], selectedeVENTS: SelectedEventsDto[]) {
+      // ACCEPT USER QUERY ONLY IF ITS FROM USER AND add selected event and train llm with these details adn fed into open ai 
+      const prompt = `train llm here`
+      const response = await this.openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [{
+            role: "user",
+            content: prompt
+        }]
+      })
+      console.log(response);
+      
+      // extract actual content
+      const content = response.choices[0].message.content;
+
+      if(!content) {
+        throw new Error("no content found in response from llm")
+      }
+
+      const jsonMatch = content.match(/\{[\s\S]*\}/);
+
+      if(!jsonMatch) {
+        throw new Error("no json found in response")
+      }
+
+      // comvert jsonMatch into json by parsing
+      const data = JSON.parse(jsonMatch[0])
+      return {
+        data: data.prompt
+    }
+    }
+}
