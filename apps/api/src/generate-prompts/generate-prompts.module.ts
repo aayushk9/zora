b5d084bed35036fbefabff735d8f460d6f9846db
Aayush <ayushkokate1@gmail.com>
2026-01-06 21:30:55 +0530

    import openai module

--- apps/api/src/generate-prompts/generate-prompts.module.ts
+++ apps/api/src/generate-prompts/generate-prompts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GeneratePromptsController } from './generate-prompts.controller';
 import { GeneratePromptsService } from './generate-prompts.service';
 import { HttpModule } from '@nestjs/axios';
+import { OpenAIModule } from 'src/openai/openai.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, OpenAIModule],
   controllers: [GeneratePromptsController],
   providers: [GeneratePromptsService]
 })
