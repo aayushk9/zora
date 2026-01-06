23fcd61f81161d5829b43fe2a5527a3fb9eb70e1
Aayush <ayushkokate1@gmail.com>
2026-01-06 21:28:01 +0530

    feat(api): backend responds with output

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -2,8 +2,8 @@ import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useEventStore } from "../store/useSelectedEventStore";
 
-interface Message {
-    role: "user" | 'agent',
+interface Messages {
+    role: "user" | 'assistant',
     content: string 
 }
 
@@ -12,7 +12,7 @@ export function useQueryHandler() {
     const location = useLocation();
     const params = new URLSearchParams(location.search);
     const incomingText = params.get("c");
-    const [messages, setMessages] = useState<Message[]>([]);
+    const [messages, setMessages] = useState<Messages[]>([]);
     const hasRun = useRef(false)
     const selectedEvents = useEventStore((s) => s.selectedEvents)
 
@@ -25,31 +25,36 @@ export function useQueryHandler() {
 
     const handleUserQuery = async (input: string) => {
         if (!input.trim()) return;
-        const userMessage: Message = { role: "user", content: input }
-        let updatedMessages: Message[] = [];
+        const userMessage: Messages = { role: "user", content: input }
+        let updatedMessages: Messages[] = [];
 
         setMessages(prev => {
             updatedMessages = [...prev, userMessage];
             return updatedMessages;
         });
+
         console.log(updatedMessages)
 
         try {
-            const res = await fetch(`http://localhost:3000/chat    `, {
+            // json body
+            const res = await fetch(`http://localhost:3000/api/chat`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ userQuery: [updatedMessages], selectedEvents })
+                body: JSON.stringify({ 
+                    messages: updatedMessages,
+                    selectedEvents 
+                })
             })
-            const output = await res.json()
-            console.log(output.data)
-            const agentMessage: Message = { role: 'agent', content: output.data }
+            // parse from json body/text to js object
+            const output = await res.text() 
+            console.log(output)
+            const agentMessage: Messages = { role: 'assistant', content: output }
             setMessages(prev => [...prev, agentMessage])
         } catch (err) {
             console.log(err)
         }
-
     }
 
     return {
