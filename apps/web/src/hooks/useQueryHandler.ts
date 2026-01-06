6b3f360a7c85efb278ed1f261c20288f8b647113
Aayush <ayushkokate1@gmail.com>
2026-01-07 04:34:49 +0530

    feat(web): create loader in messages body and add it in conversations

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -4,7 +4,8 @@ import { useEventStore } from "../store/useSelectedEventStore";
 
 interface Messages {
     role: "user" | 'assistant',
-    content: string 
+    content: string
+    isLoading?: boolean
 }
 
 export function useQueryHandler() {
@@ -25,35 +26,51 @@ export function useQueryHandler() {
 
     const handleUserQuery = async (input: string) => {
         if (!input.trim()) return;
-        const userMessage: Messages = { role: "user", content: input }
-        let updatedMessages: Messages[] = [];
 
-        setMessages(prev => {
-            updatedMessages = [...prev, userMessage];
-            return updatedMessages;
-        });
 
-        console.log(updatedMessages)
+        setMessages((prev) => [
+            ...prev,
+            { role: "user", content: input },
+            { role: "assistant", content: "", isLoading: true }
+        ])
 
         try {
-            // json body
             const res = await fetch(`http://localhost:3000/api/chat`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ 
-                    messages: updatedMessages,
-                    selectedEvents 
+                body: JSON.stringify({
+                    messages: [
+                        ...messages,
+                        { role: "user", content: input }
+                    ],
+                    selectedEvents
                 })
             })
-            // parse from json body/text to js object
-            const output = await res.text() 
+            const output = await res.text()
             console.log(output)
-            const agentMessage: Messages = { role: 'assistant', content: output }
-            setMessages(prev => [...prev, agentMessage])
+
+            setMessages((prev) => [
+
+                ...prev.slice(0, -1),
+                {
+                    role: "assistant",
+                    content: output,
+                    isLoading: false
+                }
+            ])
         } catch (err) {
-            console.log(err)
+            console.log(err);
+            setMessages((prev) => [
+
+                ...prev.slice(0, -1),
+                {
+                    role: "assistant",
+                    content: "Something went wrong. Plese try again later",
+                    isLoading: false
+                }
+            ])
         }
     }
 
@@ -61,5 +78,6 @@ export function useQueryHandler() {
         messages,
         setMessages,
         handleUserQuery,
+
     };
 }
