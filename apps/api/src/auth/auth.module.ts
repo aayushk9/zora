4268ed0120c4ebbbf7b399fce04ee9633d2ec5eb
Aayush <ayushkokate1@gmail.com>
2026-01-09 21:41:10 +0530

    import jwt module

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseService } from 'src/database/database.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+   JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: '10d'}
+   })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, DatabaseService]
 })
 export class AuthModule {}
