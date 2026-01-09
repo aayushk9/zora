a8ace9d80ccbf51fb618bb95ee9375ae7673d383
Aayush <ayushkokate1@gmail.com>
2026-01-10 03:53:16 +0530

    import cookie parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: 'http://localhost:5173',
