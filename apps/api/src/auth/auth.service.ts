37cd3f798860e91106559ddfac0d06d82e6cca70
Aayush <ayushkokate1@gmail.com>
2026-01-09 17:22:33 +0530

    some auth server logic implementation

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { SignInDto } from './dto/SignIn.dto';
+import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(){}
+    constructor(private readonly db: DatabaseService){}
 
     async signIn(user: SignInDto)  {
-     const {userName, password} = user // extract
+     const {email, password} = user // extract
 
+     // hash the password here
      /*
      Steps for authentication
      1) input validation of user -> done by dtos
@@ -19,5 +22,28 @@ export class AuthService {
                             if hashedpassword == userdb.hashedpass yes -> store user in db and generate jwt and store that jwt in cookies
                     else -> store user in db and generate jwt and than store that jwt in cookies
       */
+
+    // hash a password
+    const saltRounds = 10;
+
+     // check if user exist in database
+     const findByEmail = await this.db.query(
+        `SELECT * FROM users WHERE email = $1`,
+        [email]
+     )
+
+      const password_hash = bcrypt.hash(password, saltRounds)
+     if(findByEmail && password_hash) {
+      // generate jwt as user already exist in db
+      await this.db.query(
+        `INSERT INTO users (id, email, password_hash)
+         VALUES (gen_random_uuid(), $1, $2)
+        `, [email, password_hash]
+      )
+     } else {
+        // store user in db and than generate jwt
+
+     }
+
     }
 }
