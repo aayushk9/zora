7ecf58340ee32ce780d84e39f814643fada5ab7d
Aayush <ayushkokate1@gmail.com>
2026-01-09 21:40:31 +0530

    manul auth is active

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDto } from './dto/SignIn.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
     @Post()
-    signin(@Body() user: SignInDto) {
-     return this.authService.signIn(user)
+    async signin(
+        @Body("email") email: string,
+        @Body("password") password: string,
+        @Res({ passthrough: true }) res: Response
+    ) {
+
+        const result = await this.authService.signIn(email, password)
+
+        if (result.message === 'Invalid credentials') {
+            return res.status(401).json(result);
+        }
+
+        res.cookie('jwt', result.token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: 'strict',
+            maxAge: 10 * 24 * 60 * 60 * 1000,  // 1- days
+        });
+
+        return res.status(200).json({ message: result.message });
     }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,49 +1,51 @@
 import { Injectable } from '@nestjs/common';
-import { SignInDto } from './dto/SignIn.dto';
 import { DatabaseService } from 'src/database/database.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly db: DatabaseService){}
+  constructor(
+    private readonly db: DatabaseService,
+    private readonly jwt: JwtService
+  ) { }
 
-    async signIn(user: SignInDto)  {
-     const {email, password} = user // extract
+  async signIn(email: string, password: string) {
+    const result = await this.db.query(
+      `SELECT id, email, password_hash 
+          FROM users 
+          WHERE email = $1
+         `, [email]
+    )
 
-     // hash the password here
-     /*
-     Steps for authentication
-     1) input validation of user -> done by dtos
-     2) check if the user exists in db -> 
-                    if yes 
-                        if username == userdb.username
-                          if yes 
-                             hash the user password
-                            if hashedpassword == userdb.hashedpass yes -> store user in db and generate jwt and store that jwt in cookies
-                    else -> store user in db and generate jwt and than store that jwt in cookies
-      */
+    console.log(result)
 
-    // hash a password
-    const saltRounds = 10;
-
-     // check if user exist in database
-     const findByEmail = await this.db.query(
-        `SELECT * FROM users WHERE email = $1`,
-        [email]
-     )
-
-      const password_hash = bcrypt.hash(password, saltRounds)
-     if(findByEmail && password_hash) {
-      // generate jwt as user already exist in db
+    if (result.rowCount == 0) {
+      const password_hash = await bcrypt.hash(password, 10)
       await this.db.query(
         `INSERT INTO users (id, email, password_hash)
-         VALUES (gen_random_uuid(), $1, $2)
-        `, [email, password_hash]
+             VALUES (gen_random_uuid(), $1, $2)
+            `, [email, password_hash]
       )
-     } else {
-        // store user in db and than generate jwt
-
-     }
-
+      const payload = {email};
+      const jwt = this.jwt.sign(payload)
+      return {
+        message: "Login successful",
+        token: jwt
+      }
     }
+
+    const user = result.rows[0]
+    const validPassword = await bcrypt.compare(password, user.password_hash)
+    if (!validPassword) {
+      return { message: "Invalid credentials" }
+    }
+
+    const payload = {email};
+    const jwt = this.jwt.sign(payload)
+    return {
+      message: "Login successful",
+      token: jwt
+    }
+  }
 }
