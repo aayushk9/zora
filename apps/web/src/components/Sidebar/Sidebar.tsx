be58d7d8e24b403fba8e0b8ae8d64d9990d25b72
Aayush <ayushkokate1@gmail.com>
2026-01-14 16:07:54 +0530

    sidebar is functional and ready to use

--- apps/web/src/components/Sidebar/Sidebar.tsx
+++ apps/web/src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,58 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./Sidebar.module.css"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
-export function Sidebar () {
-    return (
-        <React.Fragment>
-           <div className={styles.parentContainer}>
-             <div className={styles.sidebar}>
-               <Link to="/query" className={styles.chat}>Research</Link>
-               <Link to="/agents" className={styles.agents}>Agents</Link>
-               <Link to="/query" className={styles.history}>History</Link> {/* for history there will be dynamic route such as /query/chatid */}
-             </div>
-             </div>
-        </React.Fragment>
-    )
+interface Conversations {
+  id: string;
+  title: string;
+}
+
+export function Sidebar() {
+  const navigate = useNavigate();
+  const [conversations, setConversations] = useState<Conversations[]>([])
+  const [historyTabOpen, setHistoryTabOpen] = useState(true)
+
+  useEffect(() => {
+    const fetchConversationOnLoad = async() => {
+      const res = await fetch("http://localhost:3000/api/conversations", {
+        method: "GET",
+        credentials: "include"
+      })
+      const data: Conversations[] = await res.json();
+      setConversations(data)
+    }
+    fetchConversationOnLoad()
+  }, [])
+
+  const openNewChat = async () => {
+    navigate(`/query`)
+  }
+  return (
+    <React.Fragment>
+      <div className={styles.parentContainer}>
+        <div className={styles.sidebar}>
+          <button onClick={() => { openNewChat()}}className={styles.chat}>
+              Research
+          </button>
+          <div className={styles.historySection}>
+           <div className={styles.historyHeader} onClick={()=> setHistoryTabOpen(prev => !prev)}>
+             <span>History</span>
+             <span className={styles.arrow}>
+                 {historyTabOpen ? "▼" : "▶"}
+              </span>
+           </div>
+           {historyTabOpen && (
+            <div className={styles.titles}>
+              {conversations.map((conversation) => (
+               <div key={conversation.id} onClick={() => navigate(`/query/${conversation.id}`)} className={styles.titleItem}>
+                  {conversation.title}
+               </div> 
+              ))}
+            </div>
+           )}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
