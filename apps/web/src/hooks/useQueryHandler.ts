bad036c18654e7ce73cd90393168646fcbfbb20a
Aayush <ayushkokate1@gmail.com>
2026-01-15 16:51:51 +0530

    handle query and conversation id creation

--- apps/web/src/hooks/useQueryHandler.ts
+++ apps/web/src/hooks/useQueryHandler.ts
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate} from "react-router-dom";
 import { useEventStore } from "../store/useSelectedEventStore";
+import { useConversationStore } from "../store/useConversationStore";
 
 interface Messages {
     role: "user" | 'assistant',
@@ -16,9 +17,26 @@ export function useQueryHandler() {
     const [messages, setMessages] = useState<Messages[]>([]);
     const hasRun = useRef(false)
     const selectedEvents = useEventStore((s) => s.selectedEvents)
+    const [conversationId, setConversationId] = useState<string>("")
+    const  conversations  = useConversationStore.getState().conversations;
+    const addConversation = useConversationStore((conversation) => conversation.addConversation)
+    const navigate = useNavigate();
+    // we need to send only once post request to /conversations from here when the user sends first request as it grabs conversation ids from there and attaches here to post request of chat so in same
+    // chat we dont need another id unless its new chat
+
+
+    // apart from intial conversation id change we will empty satte messages whenever conversationn id changes and send request to chat ednpoint with this changes conversdation id  for messages existing in that conversation id 
+    // flow => client -> api call -> db -> api call - client\
+
+   // sedn request based on 2 cases. firstly when we send the request when conversation id = 0 and when that 0 keeps changin
+
 
     useEffect(() => {
         if (incomingText && !hasRun.current) {
+            if (!incomingText) return;
+            if (!conversations) return;
+            if (hasRun.current) return;
+
             hasRun.current = true
             handleUserQuery(incomingText)
         }
@@ -29,13 +47,13 @@ export function useQueryHandler() {
 
         const userMessage: Messages = { role: "user", content: input };
         const assistantMessage: Messages = { role: "assistant", content: "", isLoading: true };
-
         const updatedMessages = [...messages, userMessage, assistantMessage];
         setMessages(updatedMessages);
 
         try {
             const res = await fetch(`http://localhost:3000/api/chat`, {
                 method: "POST",
+                credentials: "include",
                 headers: {
                     "Content-Type": "application/json"
                 },
@@ -43,15 +61,25 @@ export function useQueryHandler() {
                     messages: [
                         ...messages, userMessage
                     ],
-                    selectedEvents
+                    selectedEvents,
+                    conversationId
                 }),
-                credentials: "include"
             })
-            //const output = await res.text()
-            //console.log(output)
 
-            const data = await res.text();
-            const output = data;
+            const data = await res.json();
+            const output = data.response;
+            const id = data.id
+
+            if(!conversationId && conversations.length == 0) {
+                setConversationId(data.id)
+                navigate(`/query/${id}`)
+
+                addConversation({
+                    id: data.id,
+                    title: data.title
+                })
+            }
+            
             if (res.status == 500) {
                 alert("Something seems off please try again later")
             }
@@ -78,7 +106,6 @@ export function useQueryHandler() {
     return {
         messages,
         setMessages,
-        handleUserQuery,
-
+        handleUserQuery
     };
 }
