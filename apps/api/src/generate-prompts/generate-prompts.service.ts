550d43cddf1c3fe682a66f0707dde42096056f9c
Aayush <ayushkokate1@gmail.com>
2026-01-15 21:45:36 +0530

    replace open ai logic with groq

--- apps/api/src/generate-prompts/generate-prompts.service.ts
+++ apps/api/src/generate-prompts/generate-prompts.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { SelectedEventsDto } from './dto/selected-events.dto';
 import Groq from 'groq-sdk';
 import { GROQ_CLIENT } from 'src/groq/groq.constant';
+import { ChatCompletionMessageParam } from "groq-sdk/resources/chat/completions";
 
 @Injectable()
 export class GeneratePromptsService {
@@ -14,28 +15,49 @@ export class GeneratePromptsService {
         const prompts = `
              You are a financial analysis assistant.
 
-             Events:
-              ${JSON.stringify(selectedEvents, null, 2)}
-
              Task:    
              Generate suggested prompts a user can ask to better understand the selected market events
 
              Rules:
-                - If 1 event → generate 5 prompts for that event
-                - If multiple events → generate 1 prompt per event
-                - Maximum 5 prompts total
-                - Do NOT predict prices
-                - Do NOT give trading advice
-                - Do NOT mention specific numbers
+                - If 1 event is selected → generate 5 prompts for that event
+                - If multiple events are selected → generate 1 prompt per event
+                - Maximum return 5 prompts total
                 - Keep prompts concise
+                - return only prompts
+            for example when user selects event returt just 5 promptsx thats it no other words such as here are the prompts just return the prompts and note that
+            prompts should not begin with -
         `;
+
+        function buildSelectedEventsContext(events: SelectedEventsDto[]) {
+            if (!events || events.length === 0) {
+                return `No specific events selected.`;
+            }
+
+            return `
+        Selected Events:
+         ${events.map((e, i) => `
+           ${i + 1}. ${e.title}
+           - Category: ${e.category}
+           - Total Volume: ${e.totalVolume}
+           - Markets: ${e.marketCount}
+         `).join("")}
+        `;
+        }
+
+        const instructionsForLLM: ChatCompletionMessageParam[] = [
+            {
+                role: "system",
+                content: prompts
+            },
+            {
+                role: "system",
+                content: buildSelectedEventsContext(selectedEvents)
+            }
+        ]
 
         const response = await this.groq.chat.completions.create({
             model: "llama-3.1-8b-instant",
-            messages: [{
-                role: 'user',
-                content: prompts
-            }]
+            messages: instructionsForLLM
         })
 
         console.log("response", response)
