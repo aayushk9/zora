1d8d97158fadb0e0d26917ea2885c00f19219b36
Aayush <ayushkokate1@gmail.com>
2026-01-15 21:39:43 +0530

    replace local message state with global

---
+++ apps/web/src/store/useMessageStore.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand"
+
+interface Messages {
+    message_type: "user" | "assistant",
+    content: string,
+    isLoading?: boolean
+}
+
+interface MessagesState {
+    messages: Messages[],
+    setMessages: (message: Messages[] | ((prev: Messages[]) => Messages[])) => void,
+    addMessages: (message: Messages) => void
+}
+
+export const useMessageStore = create<MessagesState>((set) => ({
+    messages: [],
+    setMessages: (messages) => set((state) => ({
+        messages:
+            typeof messages === "function"
+                ? messages(state.messages) :
+                messages
+    })),
+    addMessages: (message) =>
+        set((state) => ({
+            messages: [message, ...state.messages]
+        }))
+}))
