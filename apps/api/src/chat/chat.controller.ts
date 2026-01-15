289b2f55fc72e8baa96b879d8b403c16aaf7c91e
Aayush <ayushkokate1@gmail.com>
2026-01-15 21:47:54 +0530

    feat(api): add history ednpoint to return exisitng conversations from table

--- apps/api/src/chat/chat.controller.ts
+++ apps/api/src/chat/chat.controller.ts
@@ -4,11 +4,12 @@ import { SelectedEventsDto } from 'src/generate-prompts/dto/selected-events.dto'
 import { Messages } from './dto/Messages';
 import type { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
+import { DatabaseService } from 'src/database/database.service';
 
 @Controller('chat')
 export class ChatController {
 
-  constructor(private readonly chatService: ChatService, private readonly jwt: JwtService) { }
+  constructor(private readonly chatService: ChatService, private readonly jwt: JwtService, private readonly db: DatabaseService) { }
 
   @Post()
   async sendResponse(
@@ -16,7 +17,8 @@ export class ChatController {
     @Body('selectedEvents') selectedEvents: SelectedEventsDto[],
     @Req() req: Request,
     @Body("conversationId") conversationId: string
-  ) {
+  ) 
+  {
     const token = req.cookies?.jwt ?? null;
 
     let userId: string | null = null;
@@ -32,5 +34,20 @@ export class ChatController {
       userId,
       conversationId
     )
+  }
+
+  @Post('history')
+  async fetchMessages(@Body("conversationId") conversationId: string) {
+     // user authenticated earlier
+
+     const result = await this.db.query(
+      `SELECT message_type, content
+       FROM messages
+       WHERE conversation_id = $1
+       ORDER BY created_at ASC
+      `, [conversationId]
+     )
+
+     return result.rows;
   }
 }
