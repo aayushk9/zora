30409ded6c23f4fb8f48e5992adfa68134cb5fd5
Aayush <ayushkokate1@gmail.com>
2026-01-17 20:42:46 +0530

    remove logs and structure code

--- apps/web/src/components/Events/Events.tsx
+++ apps/web/src/components/Events/Events.tsx
@@ -9,12 +9,14 @@ export function Events() {
 
   const [searchInput, setSearchInput] = useState("");
   const [activeCategory, setActiveCategory] = useState<Category>("all");
-  const [start, setStart] = useState(0)
-  const [end, setEnd] = useState(20)
   const [events, setEvents] = useState<EventCardProps[]>([]);
   const [loading, setLoading] = useState(true);
+  
   const addEvent = useEventStore((s) => s.addEvent);
   const selectedEvents = useEventStore((s) => s.selectedEvents)
+
+  const start = 0;
+  const end = 20;
 
   const categories: Record<Category, string> = {
     all: "All",
