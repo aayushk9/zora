c8babb197409992165eb3dda41e2a65c2fcab97a
Aayush <ayushkokate1@gmail.com>
2026-01-17 19:23:12 +0530

    fix(api): set cookie as jwt placeholder and remove auth headers in browsers

--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
+import type { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        return req?.cookies?.jwt || null;
+      },
       secretOrKey: process.env.JWT_SECRET!,
     });
   }
 
   async validate(payload: any) {
-    return { email: payload.email };
+    return { 
+      userId: payload.sub,
+      email: payload.email 
+    };
   }
 }
