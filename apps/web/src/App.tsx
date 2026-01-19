8752b0cc2360f836fc0a4c6cf01de2c790af04a5
Aayush <ayushkokate1@gmail.com>
2026-01-20 02:11:00 +0530

    change routing

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,13 +3,15 @@ import Query from './pages/Query';
 import Agents from './pages/Agents/Agents';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Zora/>} />
+          <Route path='/' element={<Home/>} />
+          <Route path='/app' element={<Zora/>}/>
           <Route path="/query" element={<Query/>} />
           <Route path='/query/:id' element={<Query/>} />
           <Route path="/agents" element={<Agents/>} />
