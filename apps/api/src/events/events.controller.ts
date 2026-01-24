448cd3383e5f81519f293b07ba6308897350c7e9
Aayush <ayushkokate1@gmail.com>
2026-01-25 04:27:41 +0530

    remove throttler from /v1/events

--- apps/api/src/events/events.controller.ts
+++ apps/api/src/events/events.controller.ts
@@ -1,17 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { EventsService } from './events.service';
-import { Throttle } from '@nestjs/throttler';
 
 @Controller({path: "events", version: "1"})
 export class EventsController {
     constructor(private readonly eventService: EventsService) {}
 
-    @Throttle({
-        default: {
-            ttl: 60,
-            limit: 60
-        }
-    })
     @Post()
     renderEvents(
         @Body('activeCategory') activeCategory: string,
