ea505c647732a520be7eeb5eeae1560cf9323de1
Aayush <ayushkokate1@gmail.com>
2026-01-26 17:03:19 +0530

    made some styling changes to nav

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -4,12 +4,13 @@ import React from 'react'
 import { Login } from '../Login/Login'
 
 export function Navbar() {
+
     return (
         <React.Fragment>
           <nav className={styles.navbar}>
-            <div> <Link className={styles.zora} to="/app"> Zora </Link></div>
+             <Link className={styles.zora} to="/app"> Zora </Link>
             <div className={styles.navRight}>
-              <Link className={styles.agents} to="/agents">Tools</Link>
+              <Link className={styles.agents} to="/agents">Tools</Link> 
               <Login />
             </div>
           </nav>
