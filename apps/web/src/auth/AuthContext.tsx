053a50ad42765d0f6e64ef264ccc91748019d9e8
Aayush <ayushkokate1@gmail.com>
2026-01-30 14:58:24 +0530

    fix: removce condition to render child as it wascausing initial black screen window

--- apps/web/src/auth/AuthContext.tsx
+++ apps/web/src/auth/AuthContext.tsx
@@ -51,7 +51,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <AuthContext.Provider value={{ user, loading, setUser, logout }}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 }
